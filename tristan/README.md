82e1e5e4b581449121c5cb9665258d4f4c62eba9
tristandevs <113070063+tristandevs@users.noreply.github.com>
2023-09-25 12:20:54 -0400

    Delete tristan directory

--- tristan/README.md
+++
@@ -1,2 +0,0 @@
-# Tristan Module
-This module enforces the use of special Tristan comments in Python code.
--- tristan/tristan/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tristan import tristanprint, enforce_tristan, start_enforcement, rainbow_text, default_comment_ending
